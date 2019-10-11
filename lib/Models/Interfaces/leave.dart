9f3bbc81f96975176eec95a4579d341b63d2925b
ashutoshsingh05 <201851029@iiitvadodara.ac.in>
2019-10-11 11:40:08 +0530

    Add methods

--- lib/Models/Interfaces/leave.dart
+++ lib/Models/Interfaces/leave.dart
@@ -1,7 +1,15 @@
 abstract class Leave {
   int remainingLeaves;
   int duration;
+  int maxContiguousPossibleDuration;
   int sanctionedLeaves();
-  bool applyLeave(int duration);
-  
+  bool applyLeave(int duration) {
+    if (duration > maxContiguousPossibleDuration)
+      return false;
+    else
+      return true;
+  }
+
+  void leavePeriodEnded() {}
+  void leavePeriodStarted() {}
 }
