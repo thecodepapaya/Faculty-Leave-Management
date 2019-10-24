49453be857ddc015752ede54088129ffcc5fe337
MayankAjmeri <mayankajmeri@gmail.com>
2019-10-24 14:28:57 +0530

    Pending Approval

--- lib/Screens/pendingApproval.dart
+++ lib/Screens/pendingApproval.dart
@@ -15,16 +15,58 @@ class _PendingApprovalState extends State<PendingApproval> {
       body: ListView(
         children: <Widget>[
           ListTile(
+            leading: Container(
+              height: 50,
+              width: 50,
+              color: Colors.red,
+              alignment: Alignment.center,
+              child: Text(
+                "M",
+                style: TextStyle(fontSize: 24, color: Colors.white),
+              ),
+            ),
             title: Text("Medical Leave"),
             subtitle: Text("For High Fever"),
           ),
           ListTile(
+            leading: Container(
+              height: 50,
+              width: 50,
+              color: Colors.blue,
+              alignment: Alignment.center,
+              child: Text(
+                "C",
+                style: TextStyle(fontSize: 24, color: Colors.white),
+              ),
+            ),
             title: Text("Casual Leave"),
+            subtitle: Text("Leave no.: 5"),
           ),
           ListTile(
+            leading: Container(
+              height: 50,
+              width: 50,
+              color: Colors.brown,
+              alignment: Alignment.center,
+              child: Text(
+                "F",
+                style: TextStyle(fontSize: 24, color: Colors.white),
+              ),
+            ),
             title: Text("Full Pay Leave"),
+            subtitle: Text("Leave no.: 3"),
           ),
           ListTile(
+            leading: Container(
+              height: 50,
+              width: 50,
+              color: Colors.green,
+              alignment: Alignment.center,
+              child: Text(
+                "C",
+                style: TextStyle(fontSize: 24, color: Colors.white),
+              ),
+            ),
             title: Text("Child Care"),
             subtitle: Text("For 12th Boards"),
           )
