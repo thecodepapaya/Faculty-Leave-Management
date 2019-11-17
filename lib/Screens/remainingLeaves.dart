38d14e5ed7794c8276fa0ab753a773f5d77a9177
anujpuri72 <anujpuri72@gmail.com>
2019-11-18 02:56:42 +0530

    linking Firebase

--- lib/Screens/remainingLeaves.dart
+++ lib/Screens/remainingLeaves.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:leave_management/Utils/LeaveScaffold.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class RemainingLeaves extends StatefulWidget {
   @override
@@ -58,206 +60,235 @@ class _RemainingLeavesState extends State<RemainingLeaves> {
   Widget build(BuildContext context) {
     return LeaveScaffold(
       title: "Remaining Leaves",
-      body: SingleChildScrollView(
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 19.0, vertical: 8.0),
-              child: Container(
-                height: 115,
-                child: Stack(
+      body: FutureBuilder(
+        future: Firestore.instance.collection("farmers").document("an").get(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting ||
+              snapshot.connectionState == ConnectionState.none) {
+            return Center(
+              child: SpinKitWanderingCubes(
+                color: Colors.red,
+              ),
+            );
+          } else {
+            if (snapshot.hasError) {
+              return Center(
+                child: Text("Error occured"),
+              );
+            } else {
+              return SingleChildScrollView(
+                child: Column(
                   children: <Widget>[
-                    Positioned(
-                      left: 40.0,
-                      child: detailsCard("MedicalLeave", Colors.red[300]),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 19.0, vertical: 8.0),
+                      child: Container(
+                        height: 115,
+                        child: Stack(
+                          children: <Widget>[
+                            Positioned(
+                              left: 40.0,
+                              child:
+                                  detailsCard("MedicalLeave", Colors.red[300]),
+                            ),
+                            Positioned(top: 16.5, child: image1),
+                          ],
+                        ),
+                      ),
                     ),
-                    Positioned(top: 16.5, child: image1),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 19.0, vertical: 1.0),
+                      child: Container(
+                        height: 115,
+                        child: Stack(
+                          children: <Widget>[
+                            Positioned(
+                              left: 40.0,
+                              child:
+                                  detailsCard("Casual Leave", Colors.blue[300]),
+                            ),
+                            Positioned(top: 16.5, child: image1),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 19.0, vertical: 8.0),
+                      child: Container(
+                        height: 115,
+                        child: Stack(
+                          children: <Widget>[
+                            Positioned(
+                              left: 40.0,
+                              child:
+                                  detailsCard("Paid Leave", Colors.green[300]),
+                            ),
+                            Positioned(top: 16.5, child: image1),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 19.0, vertical: 8.0),
+                      child: Container(
+                        height: 115,
+                        child: Stack(
+                          children: <Widget>[
+                            Positioned(
+                              left: 40.0,
+                              child: detailsCard(
+                                  "Child Care", Colors.deepPurple[200]),
+                            ),
+                            Positioned(top: 16.5, child: image1),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 19.0, vertical: 8.0),
+                      child: Container(
+                        height: 115,
+                        child: Stack(
+                          children: <Widget>[
+                            Positioned(
+                              left: 40.0,
+                              child: detailsCard(
+                                  "Paternity Leave", Colors.lightGreen[200]),
+                            ),
+                            Positioned(top: 16.5, child: image1),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 19.0, vertical: 8.0),
+                      child: Container(
+                        height: 115,
+                        child: Stack(
+                          children: <Widget>[
+                            Positioned(
+                              left: 40.0,
+                              child: detailsCard(
+                                  "Special Casual Leave", Colors.orange[200]),
+                            ),
+                            Positioned(top: 16.5, child: image1),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 19.0, vertical: 8.0),
+                      child: Container(
+                        height: 115,
+                        child: Stack(
+                          children: <Widget>[
+                            Positioned(
+                              left: 40.0,
+                              child: detailsCard(
+                                  "Vacation Leave", Colors.red[300]),
+                            ),
+                            Positioned(top: 16.5, child: image1),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 19.0, vertical: 8.0),
+                      child: Container(
+                        height: 115,
+                        child: Stack(
+                          children: <Widget>[
+                            Positioned(
+                              left: 40.0,
+                              child: detailsCard(
+                                  "Extra ordinary", Colors.blue[300]),
+                            ),
+                            Positioned(top: 16.5, child: image1),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 19.0, vertical: 8.0),
+                      child: Container(
+                        height: 115,
+                        child: Stack(
+                          children: <Widget>[
+                            Positioned(
+                              left: 40.0,
+                              child: detailsCard(
+                                  "Leave not due", Colors.green[300]),
+                            ),
+                            Positioned(top: 16.5, child: image1),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 19.0, vertical: 8.0),
+                      child: Container(
+                        height: 115,
+                        child: Stack(
+                          children: <Widget>[
+                            Positioned(
+                              left: 40.0,
+                              child:
+                                  detailsCard("Lien", Colors.deepPurple[200]),
+                            ),
+                            Positioned(top: 16.5, child: image1),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 19.0, vertical: 8.0),
+                      child: Container(
+                        height: 115,
+                        child: Stack(
+                          children: <Widget>[
+                            Positioned(
+                              left: 40.0,
+                              child: detailsCard(
+                                  "Sabattial Leave", Colors.lightGreen[200]),
+                            ),
+                            Positioned(top: 16.5, child: image1),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 19.0, vertical: 8.0),
+                      child: Container(
+                        height: 115,
+                        child: Stack(
+                          children: <Widget>[
+                            Positioned(
+                              left: 40.0,
+                              child: detailsCard(
+                                  "Special Leave", Colors.orange[200]),
+                            ),
+                            Positioned(top: 16.5, child: image1),
+                          ],
+                        ),
+                      ),
+                    )
                   ],
                 ),
-              ),
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 19.0, vertical: 1.0),
-              child: Container(
-                height: 115,
-                child: Stack(
-                  children: <Widget>[
-                    Positioned(
-                      left: 40.0,
-                      child: detailsCard("Casual Leave", Colors.blue[300]),
-                    ),
-                    Positioned(top: 16.5, child: image1),
-                  ],
-                ),
-              ),
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 19.0, vertical: 8.0),
-              child: Container(
-                height: 115,
-                child: Stack(
-                  children: <Widget>[
-                    Positioned(
-                      left: 40.0,
-                      child: detailsCard("Paid Leave", Colors.green[300]),
-                    ),
-                    Positioned(top: 16.5, child: image1),
-                  ],
-                ),
-              ),
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 19.0, vertical: 8.0),
-              child: Container(
-                height: 115,
-                child: Stack(
-                  children: <Widget>[
-                    Positioned(
-                      left: 40.0,
-                      child: detailsCard("Child Care", Colors.deepPurple[200]),
-                    ),
-                    Positioned(top: 16.5, child: image1),
-                  ],
-                ),
-              ),
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 19.0, vertical: 8.0),
-              child: Container(
-                height: 115,
-                child: Stack(
-                  children: <Widget>[
-                    Positioned(
-                      left: 40.0,
-                      child: detailsCard(
-                          "Paternity Leave", Colors.lightGreen[200]),
-                    ),
-                    Positioned(top: 16.5, child: image1),
-                  ],
-                ),
-              ),
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 19.0, vertical: 8.0),
-              child: Container(
-                height: 115,
-                child: Stack(
-                  children: <Widget>[
-                    Positioned(
-                      left: 40.0,
-                      child: detailsCard(
-                          "Special Casual Leave", Colors.orange[200]),
-                    ),
-                    Positioned(top: 16.5, child: image1),
-                  ],
-                ),
-              ),
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 19.0, vertical: 8.0),
-              child: Container(
-                height: 115,
-                child: Stack(
-                  children: <Widget>[
-                    Positioned(
-                      left: 40.0,
-                      child: detailsCard("Vacation Leave", Colors.red[300]),
-                    ),
-                    Positioned(top: 16.5, child: image1),
-                  ],
-                ),
-              ),
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 19.0, vertical: 8.0),
-              child: Container(
-                height: 115,
-                child: Stack(
-                  children: <Widget>[
-                    Positioned(
-                      left: 40.0,
-                      child: detailsCard("Extra ordinary", Colors.blue[300]),
-                    ),
-                    Positioned(top: 16.5, child: image1),
-                  ],
-                ),
-              ),
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 19.0, vertical: 8.0),
-              child: Container(
-                height: 115,
-                child: Stack(
-                  children: <Widget>[
-                    Positioned(
-                      left: 40.0,
-                      child: detailsCard("Leave not due", Colors.green[300]),
-                    ),
-                    Positioned(top: 16.5, child: image1),
-                  ],
-                ),
-              ),
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 19.0, vertical: 8.0),
-              child: Container(
-                height: 115,
-                child: Stack(
-                  children: <Widget>[
-                    Positioned(
-                      left: 40.0,
-                      child: detailsCard("Lien", Colors.deepPurple[200]),
-                    ),
-                    Positioned(top: 16.5, child: image1),
-                  ],
-                ),
-              ),
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 19.0, vertical: 8.0),
-              child: Container(
-                height: 115,
-                child: Stack(
-                  children: <Widget>[
-                    Positioned(
-                      left: 40.0,
-                      child: detailsCard(
-                          "Sabattial Leave", Colors.lightGreen[200]),
-                    ),
-                    Positioned(top: 16.5, child: image1),
-                  ],
-                ),
-              ),
-            ),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 19.0, vertical: 8.0),
-              child: Container(
-                height: 115,
-                child: Stack(
-                  children: <Widget>[
-                    Positioned(
-                      left: 40.0,
-                      child: detailsCard("Special Leave", Colors.orange[200]),
-                    ),
-                    Positioned(top: 16.5, child: image1),
-                  ],
-                ),
-              ),
-            )
-          ],
-        ),
+              );
+            }
+          }
+        },
       ),
     );
   }
