17b3f89393b20a3297e209e3550ac4017d38554a
ashutoshsingh05 <201851029@iiitvadodara.ac.in>
2019-11-17 17:15:34 +0530

    added empty files

---
+++ lib/Screens/adminDashboard.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AdminDashboard extends StatefulWidget {
+  @override
+  _AdminDashboardState createState() => _AdminDashboardState();
+}
+
+class _AdminDashboardState extends State<AdminDashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
---
+++ lib/Screens/onLeave.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class OnLeave extends StatefulWidget {
+  @override
+  _OnLeaveState createState() => _OnLeaveState();
+}
+
+class _OnLeaveState extends State<OnLeave> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
